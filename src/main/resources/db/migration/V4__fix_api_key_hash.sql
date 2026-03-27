857122b6b1e1dc3230f44887557342a9b2730120
ThatAugustus <s245764@dtu.dk>
2026-03-27 22:45:04 +0100

    api test key added in V4

--- src/main/resources/db/migration/V4__fix_api_key_hash.sql
+++ src/main/resources/db/migration/V4__fix_api_key_hash.sql
@@ -1,3 +1,5 @@
+-- Raw test API key: my-test-key-123
+-- (SHA-256 hash is stored below, use the raw key in the X-API-Key header)
 UPDATE api_keys
-SET key_hash = '625faa3fbbc3d2bd9d6ee7678d04cc5339cb33dc68d9b58451853d60046e226a'
+SET key_hash = 'fc4670b077ac29a2d622a354ecb803d55092bb3964fee64b2270f343c4270888'
 WHERE id = '22222222-2222-2222-2222-222222222222';
